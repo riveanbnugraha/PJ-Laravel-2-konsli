d3d8a155753b46d092e53930f234ab043c60e88b
kaiconima <riveannugraha04@smp.belajar.id>
2025-02-26 08:42:23 +0700

    controller, route dan blade

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+//import product controller
+use App\Http\Controllers\ProductController;
+
+//route resource for products
+Route::resource('/products', ProductController::class);
 
 Route::get('/', function () {
     return view('welcome');
